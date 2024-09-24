8c64f64b86a254dc1296eb965cdfec109b9ebf73
Alejandro Duhalde <aaduhalde@outlook.es>
2024-09-24 18:52:48 -0300

    update

--- bash/1_SCRIPTING/1_copy_apache.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Event-driven programming
-#copy proyect folder to path /var/www/html
-
-namedir=$(date +%m%d%y)
-vartime=$(date +"%T")
-
-#copys GIT --> /opt/AUTOMATA
-#copys GIT --> /var/www/HTML
